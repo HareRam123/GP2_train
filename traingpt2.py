5eeb2f580de6142a690489b4fd8d9feb94c7d552
hareram <ramganesh.ooty@gmail.com>
2026-05-01 16:10:04 +0530

    model initilisation std:0.02 and redidual as per the gpt2 (code base) release by open ai

--- traingpt2.py
+++ traingpt2.py
@@ -20,6 +20,7 @@ class CausalSelfAttention(nn.Module):
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
         # output projection
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1
         #regularization
 
         self.n_head = config.n_head
@@ -57,6 +58,7 @@ class MLP(nn.Module):
         n_embd = config.n_embd
         self.c_fc = nn.Linear(n_embd, 4 * n_embd)
         self.c_proj = nn.Linear(4 * n_embd, n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1
 
     def forward(self, x):
         x = self.c_fc(x)
@@ -94,7 +96,22 @@ class GPT(nn.Module):
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
         #weight sharing between the token embedding and the output projection layers
         self.transformer.wte.weight = self.lm_head.weight
-        
+    
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            std = 0.02
+            if hasattr(module, 'NANOGPT_SCALE_INIT') and module.NANOGPT_SCALE_INIT:
+                std *=  ( 2* self.config.n_layer) ** -0.5
+            torch.nn.init.normal_(module.weight, mean=0.0, std=std)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+
+
     def forward(self, idx, targets=None):
         device = idx.device
         b, t = idx.size()
